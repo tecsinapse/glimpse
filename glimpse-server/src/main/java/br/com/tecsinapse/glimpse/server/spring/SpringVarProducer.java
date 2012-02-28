90b420dba1b57b785787f7fae0966f1a6797b457
rui.hayashi <rui.hayashi@tecsinapse.com.br>
2012-02-28 19:03:48 -0300

    include only beans with scope singleton (issue #3)

--- glimpse-server/src/main/java/br/com/tecsinapse/glimpse/server/spring/SpringVarProducer.java
+++ glimpse-server/src/main/java/br/com/tecsinapse/glimpse/server/spring/SpringVarProducer.java
@@ -1,28 +1,36 @@
 package br.com.tecsinapse.glimpse.server.spring;
 
+import groovy.lang.Script;
+
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
 
-import groovy.lang.Script;
 import br.com.tecsinapse.glimpse.server.groovy.VarProducer;
 
 public class SpringVarProducer implements VarProducer, ApplicationContextAware {
 
-	private ApplicationContext applicationContext;
+	private ConfigurableApplicationContext applicationContext;
 	
 	public void fill(Script groovyScript) {
+		ConfigurableBeanFactory beanFactory = applicationContext.getBeanFactory();
 		groovyScript.setProperty("applicationContext", applicationContext);
 		String[] beanNames = applicationContext.getBeanDefinitionNames();
 		for (String beanName : beanNames) {
-			Object bean = applicationContext.getBean(beanName);
-			groovyScript.setProperty(beanName, bean);
+			BeanDefinition beanDef = beanFactory.getMergedBeanDefinition(beanName);
+			if (beanDef.isSingleton()) {
+				Object bean = applicationContext.getBean(beanName);
+				groovyScript.setProperty(beanName, bean);
+			}
 		}
 	}
 
 	public void setApplicationContext(ApplicationContext applicationContext)
 			throws BeansException {
-		this.applicationContext = applicationContext;
+		this.applicationContext = (ConfigurableApplicationContext) applicationContext;
 	}
 
 }
