7c99faa5196f263c906f4db5083c838230d5bafc
Rui Fernando Hayashi <rfhayashi@gmail.com>
2013-02-01 14:34:28 -0200

    implements PollOp.parse

---
+++ glimpse-server/src/main/java/br/com/tecsinapse/glimpse/server/protocol/PollOp.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012 Tecsinapse
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.tecsinapse.glimpse.server.protocol;
+
+import java.io.ByteArrayInputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import br.com.tecsinapse.glimpse.server.Server;
+
+@XmlRootElement(name="poll")
+public class PollOp implements Operation {
+
+	@XmlElement
+	private String jobId;
+
+	public PollOp() {
+	}
+
+	public PollOp(String jobId) {
+		this.jobId = jobId;
+	}
+
+	@Override
+	public Result execute(Server server) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	public static PollOp parse(String xml) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PollOp.class);
+			return (PollOp) context.createUnmarshaller().unmarshal(
+					new ByteArrayInputStream(xml.getBytes()));
+		} catch (JAXBException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+}
