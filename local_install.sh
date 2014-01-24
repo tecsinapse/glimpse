1bd7f980d179bf05569320017ebc28ba8b2e2f24
Rui Fernando Hayashi <rfhayashi@gmail.com>
2014-01-24 15:15:29 -0200

    change local_install.sh not to clean hosts file

--- local_install.sh
+++ local_install.sh
@@ -6,9 +6,9 @@ cd ..
 GLIMPSE_HOME="$HOME/.glimpse"
 if [ -d $GLIMPSE_HOME ]
 then
-    rm -rf $GLIMPSE_HOME
+    rm -rf $GLIMPSE_HOME/lib
+    rm $GLIMPSE_HOME/self-install.jar
 fi
 mkdir "$GLIMPSE_HOME"
 cp glimpse-self-install/target/self-install.jar $GLIMPSE_HOME/self-install.jar
 tar xvf glimpse-cli/target/glimpse-cli*.tar.gz -C $GLIMPSE_HOME
-echo "<hosts></hosts>" > $GLIMPSE_HOME/hosts.xml
