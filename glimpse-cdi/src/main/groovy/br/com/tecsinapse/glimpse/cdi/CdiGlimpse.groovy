f361d0749420fd2db59ef6f484ac96bc3f137862
Rui Fernando Hayashi <rfhayashi@gmail.com>
2014-04-04 11:14:32 -0300

    #15 initial skeleton cdi

---
+++ glimpse-cdi/src/main/groovy/br/com/tecsinapse/glimpse/cdi/CdiGlimpse.groovy
@@ -0,0 +1,12 @@
+package br.com.tecsinapse.glimpse.cdi
+
+import javax.inject.Named
+
+@Named
+class CdiGlimpse {
+
+    def hello() {
+        "hello"
+    }
+
+}
---
+++ glimpse-cdi/src/test/groovy/br/com/tecsinapse/glimpse/cdi/CdiGlimpseTest.groovy
@@ -0,0 +1,19 @@
+package br.com.tecsinapse.glimpse.cdi
+
+import org.jboss.weld.environment.se.Weld
+import org.jboss.weld.environment.se.WeldContainer
+import spock.lang.Specification
+
+class CdiGlimpseTest extends Specification {
+
+    def "hello"() {
+        setup:
+        def weld = new Weld()
+        WeldContainer container = weld.initialize()
+        def cdiGlimpse = container.instance().select(CdiGlimpse).get()
+
+        expect:
+        cdiGlimpse.hello() == "hello"
+    }
+
+}
